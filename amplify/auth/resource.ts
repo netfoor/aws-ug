3240c79aed61c8c87d68d2df1d2a263601ad4e42
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-10-06 22:32:13 -0600

    feat: Configure Cognito HostedIU and define user schema model in DynamoDB

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,49 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['openid', 'email', 'profile'],
+        attributeMapping: {
+          email: 'email',
+          givenName: 'given_name',
+          familyName: 'family_name',
+          profilePicture: 'picture',
+        },
+      },
+      callbackUrls: [
+        'http://localhost:3000/auth/callback',
+        'https://awspuebla.com/auth/callback',
+        'https://www.awspuebla.com/auth/callback'
+      ],
+      logoutUrls: [
+        'http://localhost:3000/',
+        'https://awspuebla.com/',
+        'https://www.awspuebla.com/'
+      ],
+    }
   },
+  userAttributes: {
+    givenName: {
+      required: true,
+      mutable: true,
+    },
+    familyName: {
+      required: true,
+      mutable: true,
+    },
+    email: {
+      required: true,
+      mutable: true,
+    },
+    phoneNumber: {
+      required: false,
+      mutable: true,
+    },
+  },
+  groups: ['Admin', 'Speaker', 'Member', 'Subscriber'],
 });
